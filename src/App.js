2d331ef5c3a84abc6d60bdcc0a6179408be599d1
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-05-09 13:04:31 +0300

    finish :)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState('');
+  const AddTodo = () => {
+    setTodo([...todo, { input, id: todo.length }]);
+    setInput('');
+  };
+  const deleteTodo = (id) => {
+    setTodo([...todo.filter((todo) => todo.id !== id)]);
+  };
   return (
     <div className="App container">
-      <div className="row justify-content-center align-content-center">
-        <div className="col-8">
-          <Navbar />
-          <AddTodo />
-          <Todos />
+      <Navbar />
+      <div className="row flex-column justify-content-center ">
+        <input
+          onChange={(e) => setInput(e.currentTarget.value)}
+          type="text"
+          placeholder="enter to-do"
+          value={input}
+        ></input>
+        <button className="btn btn-primary" onClick={AddTodo}>
+          Add Todo
+        </button>
+
+        <div className="d-flex flex-wrap">
+          {todo.map((todo) => (
+            <div className="card">
+              <div className="card-header text-center">
+                <p>To-do</p>
+              </div>
+              <div className="card-body">
+                <p className="todo">{todo.input}</p>
+                <i
+                  onClick={() => deleteTodo(todo.id)}
+                  className="fas fa-trash-alt ml-2"
+                ></i>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
